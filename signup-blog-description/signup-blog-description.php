2c6419ac0f3173dd59ac08ec76d882cc0384e96a
aaron <aaron@f53fe46e-8c02-459b-9fdd-93e75771b278>
2013-01-23 20:23:01 +0000

    - Add compatibility with New Blog Templates plugin - Clean up code a bit
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/signup-blog-description@8355 f53fe46e-8c02-459b-9fdd-93e75771b278

--- signup-blog-description/signup-blog-description.php
+++ signup-blog-description/signup-blog-description.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Set Blog Description on Blog Creation
-Plugin URI: http://premium.wpmudev.org/project/set-blog-description-on-blog-creation
+Plugin URI: http://premium.wpmudev.org/project/set-blog-description-on-blog-creation/
 Description: Allows new bloggers to be able to set their tagline when they create a blog in Multisite
-Version: 1.0.2
+Version: 1.0.3
 Author: Aaron Edwards & Andrew Billits (Incsub)
 Author URI: http://premium.wpmudev.org
 Network: true
@@ -11,7 +11,7 @@ WDP ID: 104
 */
 
 /*
-Copyright 2007-2011 Incsub (http://incsub.com)
+Copyright 2007-2013 Incsub (http://incsub.com)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License (Version 2 - GPLv2) as published by
@@ -33,20 +33,14 @@ if ( !is_multisite() )
 
 
 //------------------------------------------------------------------------//
-//---Config---------------------------------------------------------------//
-//------------------------------------------------------------------------//
-
-$default_blog_description = ''; //optional
-
-//------------------------------------------------------------------------//
 //---Hook-----------------------------------------------------------------//
 //------------------------------------------------------------------------//
 
 add_action('plugins_loaded', 'signup_blog_description_localization');
 add_action('wp_head', 'signup_blog_description_stylesheet');
-add_filter('add_signup_meta', 'signup_blog_description_meta_filter',99);
-add_action('wpmu_new_blog', 'signup_blog_description', 1, 1);
+add_filter('add_signup_meta', 'signup_blog_description_meta_filter');
 add_action('signup_blogform', 'signup_blog_description_signup_form');
+add_filter('blog_template_exclude_settings', 'signup_blog_description_nbt');
 
 //------------------------------------------------------------------------//
 //---Functions------------------------------------------------------------//
@@ -58,50 +52,17 @@ function signup_blog_description_localization() {
   load_plugin_textdomain( 'sbd', false, '/signup-blog-description/languages' );
 }
 
-function signup_blog_description($blog_ID) {
-	global $wpdb, $default_blog_description;
-	if ( ! empty($_GET['key']) ) {
-		$key = $_GET['key'];
-	} else {
-		$key = $_POST['key'];
-	}
-	if ( !empty($_POST['blog_description']) ) {
-		$blog_description = $_POST['blog_description'];
-	} else if ( !empty( $key ) ) {
-		$signup = $wpdb->get_row("SELECT * FROM " . $wpdb->signups . " WHERE activation_key = '" . $key . "'");
-		if ( empty($signup) || $signup->active ) {
-			//bad key or already active
-		} else {
-			//check for password in signup meta
-			$meta = unserialize($signup->meta);
-			if ( !empty( $meta['blog_description'] ) ) {
-				$blog_description = stripslashes($meta['blog_description']);
-			}
-		}
-	}
-
-	if ( empty($blog_description) ) {
-		$blog_description = $default_blog_description;
-	}
-
-	if ( $blog_description == 'empty' ) {
-		$blog_description = '';
-	}
-
-	if ( !empty( $blog_description ) ) {
-		switch_to_blog( $blog_ID );
-		update_option('blogdescription', stripslashes($blog_description));
-		restore_current_blog();
-	}
-}
-
 function signup_blog_description_meta_filter($meta) {
-	$blog_description = $_POST['blog_description'];
-	if ( !empty( $blog_description ) ) {
-		$add_meta = array('blog_description' => addslashes($blog_description));
-		$meta = array_merge($add_meta, $meta);
+	if ( !empty( $_POST['blog_description'] ) ) {
+		$meta['blogdescription'] = $_POST['blogdescription'];
 	}
 	return $meta;
+}
+
+/* exclude option from New Site Template plugin copy */
+function signup_blog_description_nbt( $and ) {
+	$and .= " AND `option_name` != 'blogdescription'";
+	return $and;
 }
 
 //------------------------------------------------------------------------//
@@ -121,7 +82,7 @@ function signup_blog_description_signup_form($errors) {
 	$desc = isset($_POST['blog_description']) ? esc_attr($_POST['blog_description']) : '';
 	?>
     <label for="blog_description"><?php _e('Site Tagline', 'sbd'); ?>:</label>
-		<input name="blog_description" type="text" id="blog_description" value="<?php echo $desc; ?>" autocomplete="off" maxlength="50" /><br />
+		<input name="blogdescription" type="text" id="blog_description" value="<?php echo $desc; ?>" autocomplete="off" maxlength="50" /><br />
 		<?php _e('In a few words, explain what this site is about. Default will be used if left blank.', 'sbd') ?>
 	<?php
 }
